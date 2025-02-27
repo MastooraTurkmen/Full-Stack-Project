1e7e7b406077580a451a25a1129d1ce00704b75e
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-02-27 21:39:37 +0430

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,24 +1,16 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-const passport = require("passport")
-const GoogleStrategy = require("passport-google-oauth20").Strategy
-const keys = require('./config/keys')
+require("./routes/authRoutes")(app)
+require("./services/passport")
+
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_URI)
+require("./models/User")
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
-}, (accessToken) => {
-    console.log(accessToken);
-}))
-
-app.get("/auth/google", passport.authenticate("google", {
-    scope: ["profile", "email"]
-}))
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
