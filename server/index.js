02ca84941006b41710701f96b26c898b4ec40d0d
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-01 15:31:50 +0430

    fixed port

--- server/index.js
+++ server/index.js
@@ -25,6 +25,8 @@ app.get("/", (req, res) => {
 
 require("./routes/authRoutes")(app)
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
