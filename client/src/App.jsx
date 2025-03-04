c9d026336171057184b55fb76adc88f8f5b12e26
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-04 19:20:52 +0430

    setup appjs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Header, Landing } from "./components/index";
+import Header from "./components/Header.jsx";
+import Landing from "./components/Landing.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import SurveyNew from "./components/SurveyNew.jsx";
+import { Component } from "react";
+import * as actions from "./actions";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <>
-      <h2>HI</h2>
-    </>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+    console.log(this.props.fetchUser());
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route element={<Landing />} path="landing" />
+          <Route element={<Dashboard />} path="surveys" />
+          <Route element={<SurveyNew />} path="surveys/new" />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({ user: state.auth });
+
+export default connect(mapStateToProps, actions)(App);
