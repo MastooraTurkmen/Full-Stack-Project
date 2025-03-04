3ebd8f635f256cf237c775cdc2eb572e5391462a
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-04 19:20:57 +0430

    set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers/index.jsx";
+import "materialize-css/dist/css/materialize.min.css";
+import { thunk } from "redux-thunk";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
