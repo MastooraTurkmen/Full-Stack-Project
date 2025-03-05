01166aa5ddd3829255edb73c669d0c7f95e143db
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-05 21:41:13 +0430

    web socket

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,11 +10,13 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
+        ws: true,
       },
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
+        ws: true,
       },
     },
   },
