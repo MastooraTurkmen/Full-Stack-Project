412c2121fcfa203fd9300b9bb7052974f326a3ce
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-07 19:48:55 +0430

    removed dashboard

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import Landing from "./Landing";
-import Dashboard from "./Dashboard";
 import Survey from "./Survey";
 import SurveyNew from "./SurveyNew";
 
-export { Header, Landing, Dashboard, Survey, SurveyNew };
+export { Header, Landing, Survey, SurveyNew };
