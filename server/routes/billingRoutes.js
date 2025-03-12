7213d93553a326fa57d48754b2c0c5083e209c90
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-03-12 20:28:12 +0430

    setup billing

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,5 +1,14 @@
+const stripe = require("stripe")(process.env.SECRET_KEY)
+
 module.exports = app => {
-    app.post("/api/stripe", (req, res) => {
+    app.post("/api/stripe", async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: "usd",
+            description: "$5 for 5 credits",
+            source: req.body.id
+        })
+        console.log(charge);
 
     })
 }
